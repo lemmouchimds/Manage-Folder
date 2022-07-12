4b70c6ee4ec88a73734fa2b2803a088925b4cdd2
lemmouchimds <80680784+lemmouchimds@users.noreply.github.com>
2022-07-12 11:51:13 +0100

    Add project files.

---
+++ Dir.cs
@@ -0,0 +1,62 @@
+﻿namespace Manage_Folder
+{
+    public class Dir
+    {
+        public Dir()
+        {
+
+        }
+
+        public List<string> GetExtantions(string path)
+        {
+            var result = new List<string>();
+            var files = Directory.GetFiles(path);
+
+            foreach (string file in files)
+            {
+                var extantion = Path.GetExtension(file);
+                if(!result.Contains(extantion))
+                {
+                    result.Add(extantion);
+                }
+            }
+
+            return result;
+        }
+    
+        public void CreateUnexistDirectories(string path)
+        {
+
+            //look for all directories
+            var directories = Directory.GetDirectories(path);
+
+            //look for all existing directories
+            var extantions = GetExtantions(path);
+
+            //create new directories
+            
+            foreach(var Extantion in extantions)
+            {
+                if(!directories.Contains(Extantion))
+                {
+                    Directory.CreateDirectory(path + "\\" + Extantion);
+                }
+            }
+
+        }
+
+        public void MoveFiles(string path)
+        {
+            var files = Directory.GetFiles(path);
+            var directories = Directory.GetDirectories(path);
+
+            foreach(string file in files)
+            {
+                var temp = Path.Combine(path, Path.GetExtension(file), Path.GetFileName(file));
+                Directory.Move(file, temp);
+            }
+            
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Manage_Folder;
+
+//Manage app
+Console.Write("Give me a folder : ");
+string? path = Console.ReadLine();
+
+//Directory.CreateDirectory(path + "\\hello");
+var dir = new Dir();
+dir.CreateUnexistDirectories(path);
+dir.MoveFiles(path);
+
+
+Console.WriteLine("Finished");
